ca95577fa376ae17f4625b3d9a428cf4f8c7b6c9
Gergely Máté <gerymate@gmail.com>
2015-05-03 13:01:22 +0200

    The knights and the queens know their ways.

--- source/Model/Pieces/knight.cpp
+++ source/Model/Pieces/knight.cpp
@@ -5,7 +5,28 @@ namespace Model
 
 PointerToPositionList Knight::getPossibleMoves()
 {
-    return Piece::getPossibleMoves();
+    PointerToPositionList basicPossibilities { new PositionList {
+// plane with normal y
+		position.farther().farther().left(), position.farther().farther().right(),
+		position.right().right().farther(), position.right().right().closer(),
+		position.closer().closer().right(), position.closer().closer().left(),
+		position.left().left().closer(), position.left().left().farther(),
+// plane with normal x
+		position.over().over().closer(), position.over().over().farther(),
+		position.farther().farther().over(), position.farther().farther().under(),
+		position.under().under().farther(), position.under().under().closer(),
+		position.closer().closer().under(), position.closer().closer().over(),
+// plane with normal z
+		position.over().over().left(), position.over().over().right(),
+		position.right().right().over(), position.right().right().under(),
+		position.under().under().right(), position.under().under().left(),
+		position.left().left().under(), position.left().left().over()
+	    } 
+	};
+	    
+    filterForMoveOrTake(basicPossibilities);
+	    
+    return basicPossibilities;    
 }
     
     
--- source/Model/Pieces/queen.cpp
+++ source/Model/Pieces/queen.cpp
@@ -5,7 +5,14 @@ namespace Model
     
 PointerToPositionList Queen::getPossibleMoves()
 {
-    return Piece::getPossibleMoves();
+    PointerToPositionList possibleMoves { moveOrTakeTrueDiagonallyFrom(position) };
+    PointerToPositionList straights { moveOrTakeStraightFrom(position) };
+    PointerToPositionList planeDiagonals { moveOrTakePlaneDiagonallyFrom(position) };
+    
+    possibleMoves->splice(possibleMoves->begin(), *straights);
+    possibleMoves->splice(possibleMoves->begin(), *planeDiagonals);
+    
+    return possibleMoves;
 }
     
     
